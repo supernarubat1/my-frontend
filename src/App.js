37ae6e723648681604855961dbb53d8236dfe7c5
supernarubat1 <narubat1@gmail.com>
2022-02-23 23:30:42 +0700

    add package dotenv

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+require("dotenv").config();
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
